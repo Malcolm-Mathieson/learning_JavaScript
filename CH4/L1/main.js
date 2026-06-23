340655931d1b7225f4a872d9c09a10a765c93b61
Malcolm-Mathieson <malcolm.mathieson@icloud.com>
2026-06-23 19:38:06 +0100

    CH4 L1

--- CH4/L1/main.js
+++ CH4/L1/main.js
@@ -6,12 +6,3 @@ export function createMessage(phoneNumber, message) {
     };
     return object;
 }
-
-let message = createMessage(148255510981, "Thanks for signing up");
-console.log(message);
-
-message = createMessage(148255510982, "Love to have you aboard!");
-console.log(message);
-
-message = createMessage(148255510983, "We're so excited to have you");
-console.log(message);
